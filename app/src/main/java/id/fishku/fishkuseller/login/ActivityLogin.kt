754d5c921e91d7b39bab36ac65c58ad51b2598bc
axelr <axel.rham.rus@gmail.com>
2023-05-30 06:14:44 +0700

    Test Commit

---
+++ app/src/main/java/id/fishku/fishkuseller/login/ActivityLogin.kt
@@ -0,0 +1,12 @@
+package id.fishku.fishkuseller.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.fishku.fishkuseller.R
+
+class ActivityLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
