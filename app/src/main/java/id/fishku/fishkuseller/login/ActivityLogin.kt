66a1e14016b3af5ded75db092ebfd079bd6cdb93
axelr <axel.rham.rus@gmail.com>
2023-05-30 14:30:43 +0700

    Rapihin id XML Login Register

--- app/src/main/java/id/fishku/fishkuseller/login/ActivityLogin.kt
+++ app/src/main/java/id/fishku/fishkuseller/login/ActivityLogin.kt
@@ -3,10 +3,19 @@ package id.fishku.fishkuseller.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.fishku.fishkuseller.R
+import id.fishku.fishkuseller.databinding.ActivityLoginBinding
 
 class ActivityLogin : AppCompatActivity() {
+
+    private lateinit var loginBinding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        loginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(loginBinding.root)
+
+        loginBinding.apply{
+
+        }
+
     }
 }
