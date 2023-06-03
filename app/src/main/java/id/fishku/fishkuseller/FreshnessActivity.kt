ec6abda5aa8d761caa74898a70504896c34ccfdb
Jelita <jelita.mantika@gmail.com>
2023-06-03 15:49:52 +0700

    tambah packing dan freshness

---
+++ app/src/main/java/id/fishku/fishkuseller/FreshnessActivity.kt
@@ -0,0 +1,11 @@
+package id.fishku.fishkuseller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FreshnessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_freshness)
+    }
+}
