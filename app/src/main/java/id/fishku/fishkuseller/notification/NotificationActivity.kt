f8213b3141cbf332750d6b9aa618645927ef03e1
axelr <axel.rham.rus@gmail.com>
2023-06-06 15:25:33 +0700

    Changes : Apus Fragment News DLL

--- app/src/main/java/id/fishku/fishkuseller/notification/NotificationActivity.kt
+++ app/src/main/java/id/fishku/fishkuseller/notification/NotificationActivity.kt
@@ -2,28 +2,23 @@ package id.fishku.fishkuseller.notification
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.tabs.TabLayoutMediator
+import androidx.recyclerview.widget.LinearLayoutManager
 import id.fishku.fishkuseller.databinding.ActivityNotificationBinding
-import id.fishku.fishkuseller.notification.adapter.NotificationPagerAdapter
+import id.fishku.fishkuseller.notification.adapter.NotificationOrderAdapter
 
 class NotificationActivity : AppCompatActivity() {
 
-    private lateinit var notifBinding : ActivityNotificationBinding
+    private lateinit var notifBinding: ActivityNotificationBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         notifBinding = ActivityNotificationBinding.inflate(layoutInflater)
         setContentView(notifBinding.root)
 
-        val pagerAdapter = NotificationPagerAdapter(this)
-        with(notifBinding){
-            viewPager.adapter = pagerAdapter
-            TabLayoutMediator(tabs, viewPager){tabs,position ->
-                when (position){
-                    0 -> tabs.text = "Orders"
-                    1 -> tabs.text = "News"
-                }
-            }.attach()
+        notifBinding.apply {
+            rvNotifOrder.layoutManager = LinearLayoutManager(this@NotificationActivity)
+            rvNotifOrder.adapter = NotificationOrderAdapter()
         }
+
     }
 }
